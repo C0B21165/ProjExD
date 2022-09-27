83321b56064aeca0df03247a976aab789fba97a3
Leona <c0b2116596@edu.teu.ac.jp>
2022-09-27 17:05:44 +0900

    alphabetさがし

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -5,10 +5,10 @@ kes = 2
 
 zennbu = [
     "A","B","C","D","E","F","G","H","I","J","K","L","M",\
-    "N","O","P","Q","R","S","T","U","V","v","W","X","Y","Z"
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
     ]
 
-taisyou = ["A", "F", "D", "G", "H", "S","W"]#26文字からsampleでとる
+taisyou = (random.sample(zennbu, tai))#26文字からsampleでとる
 (random.shuffle(taisyou))
 print("対象文字:", end = "")
 for i in taisyou:
@@ -16,10 +16,37 @@ for i in taisyou:
 print("")
 
 a = (random.sample(taisyou, kes))
+b = a[0]
+c = a[1]
+
 
 result = list(set(taisyou) - set(a))
 
-print("欠損文字:", end = "")
+print("表示文字:", end = "")
 for j in result:
     print(j + " ", end = "")
 
+print("")
+count = 0
+while count < 11:
+    ans1 = int(input("欠損した文字数は？"))
+    if ans1 == kes:
+        print("正解です！では欠損文字は何でしょうか？一文字ずつ答えてください。")
+        break
+    else:
+        print("不正解") 
+        count += 1
+        continue
+        ans2 = input("一つ目")
+        if ans2 == b:
+            print("正解")
+            break
+        else:
+            print("不正解。もう一回。")
+
+        ans3 = input("二つ目")
+        if ans3 == c:
+            print("正解")
+            break
+        else:
+            print("不正解。もう一回。")
