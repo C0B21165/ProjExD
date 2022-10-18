8cb0001075ea07806cb932b6bd325f8ae10deafc
Leona <c0b2116596@edu.teu.ac.jp>
2022-10-18 14:19:40 +0900

    練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,6 +3,15 @@ from turtle import title
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")#1
+
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")#2
+    canv.pack()
+    
+
+    tori = tk.PhotoImage(file="fig/9.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag = "tori")
+
 
     root.mainloop()
