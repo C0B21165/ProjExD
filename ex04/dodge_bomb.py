626a53bfe32f49dba2de2aeeb8ef06d2c5b59be8
Leona <c0b2116596@edu.teu.ac.jp>
2022-10-25 14:39:02 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,20 +1,30 @@
 import pygame as pg
 
 def main():
-    pg.display.set_caption("逃げろ、こうかとん")#連取1
+    pg.display.set_caption("逃げろ！こうかとん")#連取1
     scrn_sfc = pg.display.set_mode((1600, 900))
-
-    bg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bg_sfc = pg.image.load("fig/fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
+
+    #3
+    tori_sfc = pg.image.load("fig/fig/7.jpg")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
     clock = pg.time.Clock()
 
+    #2
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
-        pg.display.update()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+
+        #3
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        pg.display.update()
         
         clock.tick(1000)
 
