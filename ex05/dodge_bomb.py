282a69dcc73f1a98aadd356e0f5ea8c282e92c18
Leona <c0b2116596@edu.teu.ac.jp>
2022-11-01 17:09:05 +0900

    最終一応保存

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -1,6 +1,32 @@
+from typing_extensions import Self
 import pygame as pg
 import sys
 from random import randint
+
+import time
+
+#追加機能1
+#class sounds:
+    #def alarm(self, son):
+        #pg.mixer.init(frequency = 44100) 
+        #self.pg.mixer.music.load(son)
+
+
+#追加機能1
+#class Sound:
+    #def __init__(self, son):
+        #self.son = son
+        #pg.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=512)
+        #pg.mixer.init()
+        #self.sound1 = pg.mixer.Sound(son)
+
+#追加機能1
+def sound():
+    pg.mixer.init(frequency = 44100)
+    pg.mixer.music.load("carstopcrash.wav")   
+    pg.mixer.music.play(-1)            
+    time.sleep(1000)                    
+    pg.mixer.music.stop() 
 
 class Screen:
     def __init__(self, title, wh, bgimg):
@@ -77,7 +103,76 @@ def check_bound(obj_rct, scr_rct):
     return yoko, tate
 
 
+
+
+class Bird2:
+    key_delta = {
+        pg.K_w:    [0, -1],
+        pg.K_s:  [0, +1],
+        pg.K_a:  [-1, 0],
+        pg.K_d: [+1, 0],
+    }
+
+    def __init__(self, img, zoom, xy):
+        sfc = pg.image.load(img) # "fig/6.png"
+        self.sfc = pg.transform.rotozoom(sfc, 0, zoom) # 2.0
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy # 900, 400
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        key_states = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_states[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+                if check_bound(self.rct, scr.rct) != (+1, +1):
+                    self.rct.centerx -= delta[0]
+                    self.rct.centery -= delta[1]
+        self.blit(scr)
+
+
+class Bomb2:
+    def __init__(self, color, radius, vxy, scr:Screen):
+        self.sfc = pg.Surface((radius*2, radius*2))
+        self.sfc.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.sfc, color, (radius, radius), radius)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centery = randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+def check_bound(obj_rct, scr_rct):
+    """
+    obj_rct：こうかとんrct，または，爆弾rct
+    scr_rct：スクリーンrct
+    領域内：+1／領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
 def main():
+
+    #son = Sound("carstopcrash.wav")
+
+
     scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
 
 
@@ -85,6 +180,9 @@ def main():
 
 
     bkd = Bomb((255, 0, 0), 10, (+1, +1), scr)
+
+
+    #a = sounds("carstopcrash.wav")
 
     clock = pg.time.Clock()
     while True:
@@ -94,7 +192,6 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-
         kkt.update(scr)
 
         # 練習7
@@ -103,6 +200,11 @@ def main():
         # 練習8
         if kkt.rct.colliderect(bkd.rct):
             return
+        
+        #sound()
+
+
+
 
         pg.display.update()
         clock.tick(1000)
